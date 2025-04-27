146ec0d97c8e95d47bb8ac8554ff5e2ccd9e4baf
solunara <soluna@yeah.net>
2025-04-27 23:55:42 +0800

    添加高德地图初始页面

---
+++ src/views/vehicle/maplocation/MapLocation.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="container">
+
+    </div>
+</template>
+
+<script setup>
+import AMapLoader from '@amap/amap-jsapi-loader'
+import apiKey from '@config/apikey.js'
+function initMap(){
+    AMapLoader.load({
+        key: apiKey,
+    })
+    .then(AMap => {
+        var map = new AMap.map('container', {
+            zoom: 11, // 初始化地图缩放层级
+            center: [116.397428, 39.90923], // 初始化显示的中心位置：北京天安门
+            viewMode: '3D',
+            resizeEnable: true, // 监控地图尺寸变化
+        })
+    })
+    .catch(err => {
+        console.log(err);
+    })
+}
+initMap()
+</script>
+
+<style lang="scss" scoped>
+#container {
+    width: 100%;
+    height: 100%;
+}
+</style>
