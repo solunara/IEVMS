00119419f9ebd8f7959146e7214b69615722d13c
solunara <soluna@yeah.net>
2025-04-27 10:53:30 +0800

    增加element-plus的全局语言配置

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <script setup>
-
+import { reactive } from 'vue'
+import { ElConfigProvider } from 'element-plus' // 配置全局element-plus组件语言
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+const { locale } = reactive({
+    locale: zhCn
+})
 </script>
 
 <template>
-    <router-view />
+    <el-config-provider :locale="locale">
+        <router-view />
+    </el-config-provider>
 </template>
 
 <style scoped>
